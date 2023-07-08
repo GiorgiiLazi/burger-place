1a2b8b44c21d4de6d2915130816de8838806993b
george <malazi140@gmail.com>
2023-07-08 13:32:02 +0300

    added company info, footer and scroll

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const navItems = document.querySelector('.nav-items')
 const btnColor = document.querySelector('.btn-color')
 const btnDay = document.querySelector('.btn-day')
 const btnDark = document.querySelector('.btn-dark')
+const up = document.querySelector('.up')
+const down = document.querySelector('.down')
 
 // navbar toggle
 navToggle.addEventListener('click', ()=>{
@@ -28,3 +30,15 @@ btnColor.addEventListener("click", ()=>{
         body.style.color = "black"
     }
 })
+
+// scroll up/down
+
+up.addEventListener('click', ()=>{
+    scrollTo({top:0, behavior: 'smooth'})
+})
+down.addEventListener('click', ()=>{
+    scrollTo({top: document.body.scrollHeight, behavior: 'smooth' })
+})
+
+
+
